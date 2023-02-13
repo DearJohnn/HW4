6153e6c2d02de11b67f762c4607bb70b00bc5e9a
DearJohnn <sun.yuch@northeastern.edu>
2023-02-13 17:48:50 -0500

    Initial commit

---
+++ RaceTo21_W3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RaceTo21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardTable cardTable = new CardTable();
+            Game game = new Game(cardTable);
+            while (game.nextTask != Task.GameOver)
+            {
+                game.DoNextTask();
+            }
+        }
+        /********* Draft *********
+         
+         *Level 1: 
+                    Design a chip account for each player. Players can bet each round, and the winner wins all the chips. 
+                    The player's bet is kept for the next turn if no one wins this turn.
+         *Level 2：
+                   The person with the most bets in the wager account after three rounds wins, and if a player loses all 
+                   bets within a period of 3 rounds, the game ends immediately. 
+         
+         
+         */
+    }
+}
+
